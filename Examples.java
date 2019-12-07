12d2b6c9014c651ba24f5730021fabcbeda5a9f1
Danila Papko <36470704+dapapko@users.noreply.github.com>
2019-12-07 15:10:55 +0300

    Add files via upload

--- Examples.java
+++ Examples.java
@@ -1,31 +1,30 @@
 public class Examples {
     public static void main(String[] args) {
-        try {
-            ExtendableStack st1 = new ExtendableStack();
-            st1.push(10);
-            st1.push(20);
-            st1.push(30);
-            st1.push(40);
-            st1.push(50);
-            st1.push(60);
-            st1.pop();
-            st1.pop();
-            st1.pop();
-            StackWithPointer st2 = new StackWithPointer(3);
-            try {
-                st2.push(10);
-                st2.push(20);
-                st2.push(30);
-                st2.push(40);
-            } catch(StackOverflowException exc) {
-                System.out.println(exc.getMessage());
-            }
-            st2.pop();
-            st2.pop();
-            System.out.println(st2.peek());
-        } catch (EmptyStackException exc) {
-            System.out.println(exc.getMessage());
-        }
+        String exp = "(8+7)+(9*4))";
+        boolean isValid = bracketsValidation(exp);
+        if (isValid) {
+            System.out.println("Valid");
+        } else {System.out.println("Invalid"); }
     }
-
+public static char[] stringToListOfChars(String str) {
+    char[] expList = new char[str.length()];
+    for(int i=0; i< str.length();i++) {
+        expList[i] = str.charAt(i);
+    }
+    return expList;
+}
+public static boolean bracketsValidation(String expression){
+      char[] strList = expression.toCharArray();
+      ExtendableStack stack = new ExtendableStack();
+      for(int j=0; j< strList.length;j++) {
+          if(strList[j] == '(') {
+              stack.push(7);
+          } else if (strList[j] == ')' ) {
+              try {
+                  stack.pop();
+              } catch (EmptyStackException exc) {return false;}
+          }
+      }
+    return stack.getLength() == 0;
+}
 }
