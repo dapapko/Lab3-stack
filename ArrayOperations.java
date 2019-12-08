938c06460a7828f239852e8e65ab315afbd8662f
Danila Papko <36470704+dapapko@users.noreply.github.com>
2019-12-08 22:20:35 +0300

    Update ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -19,7 +19,8 @@ public  class ArrayOperations {
         newarr[arr.length] = item;
         return newarr;
     }
-public static int[] extend(int[] arr, int delta) {
+public static int[] extend(int[] arr) {
+int delta = arr.length/2 +1;
 int[] newarr = new int[arr.length+delta];
 for (int i = 0; i < arr.length; i++) {
             newarr[i] = arr[i];
