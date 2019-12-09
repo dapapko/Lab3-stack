8a4b400f9fd83a26d7727b7bf9fe8ce2349f3aa4
Danila Papko <36470704+dapapko@users.noreply.github.com>
2019-12-09 21:15:47 +0300

    Adding reverse polish notation calculator

--- Examples.java
+++ Examples.java
@@ -8,13 +8,66 @@ public class Examples {
             st1.push(40);
             st1.push(50);
             st1.pop();
+            int result = reversePolishNotationCalculator("23+4*");
+            System.out.println("Result: " + result);
             int last = st1.peek();
             System.out.println(last);
         } catch (StackOverflow | EmptyStack ex) {
             System.out.println(ex.getMessage());
         }
     }
+    public static boolean In(char[] arr, char item) {
+        for(int i=0;i<arr.length;i++) {
+            if (arr[i] == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int reversePolishNotationCalculator(String expression) throws StackOverflow, EmptyStack {
+        int counter =0;
+        char[] operators = new char[]{'+', '-', '*', '/'};
+        ExtendableStackWithPointer stack = new ExtendableStackWithPointer();
+        char[] expArray = expression.toCharArray();
+        for(int i=0; i< expArray.length;i++){
+            if (!In(operators, expArray[i] )) {
+                int number = Character.getNumericValue(expArray[i]);
+                stack.push(number);
+                counter++;
+            } else {
+                int res = 0;
+                if(expArray[i] == '+') {
+                    for(int k=0;k<counter;k++) {
+                        res += stack.pop();
+                    }
+                    stack.push(res);
+                    counter =0;
+                } else if (expArray[i] == '-') {
+                    for(int k=0;k<counter;k++) {
+                        res -= stack.pop();
+                    }
+                    stack.push(res);
+                    counter =0;
+                }  else if (expArray[i] == '*') {
+                    res = stack.pop();
+                    for(int k=0;k<counter;k++) {
+                        res *= stack.pop();
+                    }
+                    stack.push(res);
+                    counter =0;
+                }  else if (expArray[i] == '/') {
+                    res = stack.pop();
+                    for(int k=0;k<counter;k++) {
+                        res /= stack.pop();
+                    }
+                    stack.push(res);
+                    counter =0;
+                }
 
+            }
+        }
+        return stack.pop();
+    }
     public static char[] stringToListOfChars(String str) {
         char[] expList = new char[str.length()];
         for (int i = 0; i < str.length(); i++) {
