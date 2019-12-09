56cac6f6c5270753509e0e5f86727194e6034220
Danila Papko <36470704+dapapko@users.noreply.github.com>
2019-12-09 17:11:08 +0300

    Delete StackOverflowException.java

--- StackOverflowException.java
+++
@@ -1,5 +0,0 @@
-public class StackOverflowException extends Exception {
-        public StackOverflowException () {
-            super("Stack overflow. You should delete some elements from the stack.");
-        }
-    }
