3a633585bfe8fa706238bc27d6919e332149c6f4
Danila Papko <36470704+dapapko@users.noreply.github.com>
2019-12-12 16:40:48 +0300

    Adding permutations

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // можно было бы создать вложенный статический класс, но я решил
 // вынести эти операции в отдельный статический класс, поскольку они могут пригодиться в дальнейшем.
 public class ArrayOperations {
@@ -8,12 +10,80 @@ public class ArrayOperations {
         }
         return newarr;
     }
+    public static String[] deleteLastStringFromArray(String[] arr) {
+        String[] newarr = new String[arr.length - 1];
+        for (int i = 0; i < newarr.length; i++) {
+            newarr[i] = arr[i];
+        }
+        return newarr;
+    }
 
-    public static int[] appendToArray(int[] arr, int item) throws StackOverflow {
+    public static int[] appendToArray(int[] arr, int item) throws StackOverflowError {
         if (arr.length + 1 > Integer.MAX_VALUE - 2) {
-            throw new StackOverflow();
+            throw new StackOverflowError();
         }
         int[] newarr = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];
+        }
+        newarr[arr.length] = item;
+        return newarr;
+    }
+
+    public static int[] appendToIntegerArray(int[] arr, int item)  {
+        if (arr.length + 1 > Integer.MAX_VALUE - 2) {
+            throw new StackOverflowError();
+        }
+        int[] newarr = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];
+        }
+        newarr[arr.length] = item;
+        return newarr;
+    }
+    public static int[] appendToArr(int[] arr, int item) {
+        int[] newarr = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];
+        }
+        newarr[arr.length] = item;
+        return newarr;
+    }
+    public static boolean In(int[]a, int item) {
+        for (int i=0; i<a.length;i++) {
+            if (a[i] == item) {return true;}
+        }
+        return false;
+    }
+    public static int[] deleteFromArrayByValue(int[] arr, int value) {
+        int[] newarr = new int[0];
+        for(int i=0;i<arr.length;i++) {
+            if (arr[i] != value) {
+               newarr = appendToArr(newarr, arr[i]);
+            }
+        }
+        return newarr;
+    }
+    public static void printArr(int[] a) {
+        for (int i =0; i<a.length;i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+public  static int[] difference(int[]a, int[]b) {
+        int[] newarr = new int[0];
+        for(int i=0;i<a.length;i++) {
+            if  (!In(b, a[i])) {
+                newarr = appendToArr(newarr, a[i]);
+            }
+        }
+        Arrays.sort(newarr);
+        return newarr;
+}
+    public static String[] appendStringToArray(String[] arr, String item) throws StackOverflowError {
+        if (arr.length + 1 > Integer.MAX_VALUE - 2) {
+            throw new StackOverflowError();
+        }
+        String[] newarr = new String[arr.length + 1];
         for (int i = 0; i < arr.length; i++) {
             newarr[i] = arr[i];
         }
@@ -27,5 +97,29 @@ public class ArrayOperations {
             newarr[i] = arr[i];
         }
         return newarr;
+    }
+
+    public static String[] extendString(String[] arr) {
+        int delta = arr.length/2 +1;
+        String[] newarr = new String[arr.length+delta];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];
+        }
+        return newarr;
+    }
+    public static char[] extendChar(char[] arr) {
+        int delta = arr.length/2 +1;
+        char[] newarr = new char[arr.length+delta];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];
+        }
+        return newarr;
+    }
+    public static int[] copy(int[] arr) {
+        int[] copy = new int[arr.length];
+        for(int i=0;i<arr.length;i++) {
+            copy[i] = arr[i];
+        }
+        return copy;
     }
 }
---
+++ Permutations.java
@@ -0,0 +1,175 @@
+import java.util.Random;
+
+class Permutations {
+    public static int index(int[] arr, int item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] shuffle(int[] arr) {
+        Random rand = new Random();
+        int[] shuffled = new int[0];
+        while (arr.length > 1) {
+            int index = rand.nextInt(arr.length - 1);
+            int item = arr[index];
+            shuffled = ArrayOperations.appendToArr(shuffled, item);
+            arr = ArrayOperations.deleteFromArrayByValue(arr, item);
+        }
+        shuffled = ArrayOperations.appendToArr(shuffled, arr[0]);
+        return shuffled;
+    }
+
+    public static boolean countOrNot(int[] permutation) {
+        int check = permutation[0];
+        for (int j = 0; j < permutation.length; j++) {
+            if (permutation[j] != check) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void indexesToMembers(int[] array, int[] indexes) {
+        for (int i = 0; i < indexes.length; i++) {
+            System.out.print(array[indexes[i]] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] getRandomArray(int length) {
+        Random rand = new Random();
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = rand.nextInt();
+        }
+        return arr;
+    }
+
+    public static int[] getLoop(int[] sequence, int[] permutation) {
+        int[] loop = new int[0];
+        int index1 = index(permutation, sequence[0]);
+        loop = ArrayOperations.appendToArr(loop, sequence[0]);
+        loop = ArrayOperations.appendToArr(loop, index1);
+        int src = index1;
+        while (true) {
+            if (loop[0] == loop[loop.length - 1]) {
+                return loop;
+            }
+            int index = index(permutation, src);
+            loop = ArrayOperations.appendToArr(loop, index);
+            src = index;
+        }
+    }
+
+    public static int countLoops(int[] sequence, int[] permutation) {
+        int loopCounter = 0;
+        while (sequence.length > 1) {
+            int[] loop = getLoop(sequence, permutation);
+            sequence = ArrayOperations.difference(sequence, loop);
+            if (countOrNot(loop)) {
+                loopCounter++;
+            }
+        }
+        return loopCounter;
+    }
+
+    public static void printLoops(int[] sequence, int[] permutation, int[] array) {
+        int counter = 0;
+        while (sequence.length > 1) {
+            int[] loop = getLoop(sequence, permutation);
+            sequence = ArrayOperations.difference(sequence, loop);
+            if (countOrNot(loop)) {
+                indexesToMembers(array, loop);
+                counter++;
+            }
+        }
+        System.out.println("Loops count: " + counter);
+    }
+
+    public static void generatePermutationWithOneLoop(int[] sequence, int[] array) {
+        while (true) {
+            int[] sequenceCopy = shuffle(sequence);
+            int counter = countLoops(sequence, sequenceCopy);
+            if (counter == 1) {
+                System.out.println("Initial sequence:");
+                ArrayOperations.printArr(array);
+                System.out.println();
+                System.out.println("Permutation:");
+                indexesToMembers(array, sequenceCopy);
+                System.out.println();
+                System.out.println("Loop:");
+                int[] loop = getLoop(sequence, sequenceCopy);
+                indexesToMembers(array, loop);
+                System.out.println();
+                break;
+            }
+        }
+    }
+
+    public static void countAndPrintLoops(int[] array, int[] permutation) {
+        int[] sequence = new int[array.length];
+        for (int i = 0; i < sequence.length; i++) {
+            sequence[i] = i;
+        }
+        printLoops(sequence, permutation, array);
+    }
+
+    public static void permutationWithGivenNumberOfLoops(int[] array, int loops) {
+        int[] sequence = getArrayOfIndexes(array);
+        int[] sequenceCopy = ArrayOperations.copy(sequence);
+        while (true) {
+            sequenceCopy = shuffle(sequenceCopy);
+            int loopsCount = countLoops(sequence, sequenceCopy);
+            if (loopsCount == loops) {
+                System.out.println("Permutation: ");
+                indexesToMembers(array, sequenceCopy);
+                System.out.println("Loops: ");
+                printLoops(sequence, sequenceCopy, array);
+                break;
+            }
+            sequenceCopy = sequence;
+        }
+    }
+
+    public static int[] getArrayOfIndexes(int[] arr) {
+        int[] indexes = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            indexes[i] = i;
+        }
+        return indexes;
+    }
+
+    public static void randperm() {
+        int[] arr = new int[]{7, 8, 9, 10, 11, 12, 16};
+        System.out.println("Initial sequence");
+        ArrayOperations.printArr(arr);
+        int[] sequence = getArrayOfIndexes(arr);
+        int[] shuffled = shuffle(sequence);
+        System.out.println();
+        System.out.println("Permutation:");
+        indexesToMembers(arr, shuffled);
+        System.out.println();
+        System.out.println("Loops count: ");
+        printLoops(sequence, shuffled, arr);
+    }
+    public static void oneloop() {
+        int[] arr = new int[]{7, 8, 9, 10, 11, 12, 16};
+        int[] sequence = getArrayOfIndexes(arr);
+        generatePermutationWithOneLoop(sequence, arr);
+    }
+    public static void randarrperm(int length, int loops) {
+        if(length < 2 ) {
+            System.out.println("Length must be more than 1");
+            return;
+        }
+        int[] arr = getRandomArray(length);
+        System.out.println("Array:");
+        ArrayOperations.printArr(arr);
+        System.out.println();
+        permutationWithGivenNumberOfLoops(arr, loops);
+    }
+}
